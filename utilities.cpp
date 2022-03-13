0f4830ca109b92cb431c17397e7dd3d8f83fc6c1
mark-smithson <63368659+mark-smithson@users.noreply.github.com>
2022-03-13 17:24:52 +0100

    Update utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -1,5 +1,12 @@
-#include "utilites.h"
+#include 'utilities.h'
+using namespace std;
 
-int binarySearch(const vector<int>& nodo) {
-
+int binarySearch(const vector<int>& node, int left, int right, int v) {
+    if (left <= right) {
+        int mid = (left+right)/2;
+        if (node[mid] == v) return mid;
+        else if (v < node[mid]) return binarySearch(node,left,mid - 1,v);
+        else return binarySearch(node,mid + 1,right,v);
+    }
+    return -1;
 }
