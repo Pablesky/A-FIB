f1b73ff96ed7b3ffcd2a9e8ef468c15495b6f8b3
mark-smithson <63368659+mark-smithson@users.noreply.github.com>
2022-03-14 17:50:27 +0100

    Update mainGraph.cpp

--- mainGraph.cpp
+++ mainGraph.cpp
@@ -12,7 +12,7 @@ int main() {
     Graph G(n);
     G.read();
     G.get_connected_components();
-    cout << G.num_cc() << endl;
+    cout << "THE NUM OF CCs is : " << G.num_cc() << endl;
     cout << endl;
     G.write_CCs();
     G.get_complex_ccs();
@@ -23,6 +23,20 @@ int main() {
         else cout << "CC : " << i << " is NOT complex" << endl;
     }
 
+    cout << "------------- DELETING -------------" << endl;
+    G.delete_vertex(3);
+    cout << "VERTEX DELETED" << endl;
+    G.get_connected_components();
+    cout << " THE NUM OF CCs is : " << G.num_cc() << endl;
+    cout << endl;
+    G.write_CCs();
+    G.get_complex_ccs();
+    cout << "NUMBER OF CCS: " << G.num_cc() << endl;
+    cout << endl;
+    for (int i = 0; i < G.num_cc(); ++i) {
+        if (G.is_complex(i)) cout << "CC : " << i << " is complex" << endl;
+        else cout << "CC : " << i << " is NOT complex" << endl;
+    }
 }
 */
 
